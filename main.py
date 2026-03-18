a744fc2dca63aa54f27836ac1073383f24dca9a7
Jakub <jstepien257@gmail.com>
2026-03-18 21:52:59 +0100

    added login test

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-@app.get("/")
-def root():
-    return {"message": "API działa"}
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+class LoginData(BaseModel):
+    username: str
+    password: str
+
+
+@app.post("/login")
+def login(data: LoginData):
+    if data.username == "admin" and data.password == "1234":
+        return {"success": True}
+
+    raise HTTPException(
+        status_code=401,
+        detail="Nieprawidłowy login lub hasło"
+    )
